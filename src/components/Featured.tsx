efb37f6c2757bddc113ff7e10af11c4598126b3a
Areeba Majid <160718011+Areeba-majid@users.noreply.github.com>
2024-12-19 22:22:02 +0500

    Update Featured.tsx

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -30,20 +30,20 @@ const products = [
 export default function Featured() {
   return (
     <div className="w-full flex flex-col p-2 mt-10">
-      {/* Title */}
+      
       <h1 className="text-2xl font-bold p-2">Featured Products</h1>
       
-      {/* Product Grid */}
+      
       <div className="w-full flex flex-col items-center mt-5 gap-4 md:flex-row md:justify-center md:gap-10">
         {products.map((product) => (
           <div key={product.id} className="flex flex-col w-full md:w-1/4 items-center">
-            {/* Product Item */}
+            
             <div className="relative">
-              {/* "New" Tag */}
+              
               <span className="absolute bg-[#01AD5A] p-1 text-white ml-2 mt-4 text-sm rounded-lg">
                 New
               </span>
-              {/* Product Image */}
+      
               <Image
                 alt={product.title}
                 width={250}
@@ -54,7 +54,7 @@ export default function Featured() {
               />
             </div>
 
-            {/* Product Title and Price */}
+            
             <div className="flex flex-col items-center mt-4">
               <h2 className="font-bold">{product.title}</h2>
               <div className="flex justify-between p-2 items-center w-full">
